df48ca57424df46d35f603a760935c0c9871fc66
Matheus Antonio <matheusantonio232@gmail.com>
2021-12-06 13:18:15 -0300

    feat: add categories migration

--- src/migrations/20211206155717-create-users.js
+++ src/migrations/20211206155717-create-users.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("Users", {
+    const usersTable = await queryInterface.createTable("Users", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -32,6 +32,8 @@ module.exports = {
         field: "image",
       },
     });
+
+    return usersTable;
   },
 
   down: async (queryInterface, Sequelize) => {
