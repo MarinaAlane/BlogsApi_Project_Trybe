f64080c072f2e8ae73ef3080723a14443963b376
Matheus Antonio <matheusantonio232@gmail.com>
2021-12-06 18:23:09 -0300

    feat: add get users route

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -2,11 +2,14 @@ const router = require('express').Router();
 const controller = require('../controllers/userController');
 const { validateDisplayName, validateEmail, 
   validatePassword } = require('../middlewares/userValidation');
+  const { validateToken } = require('../middlewares/validateToken');
 
 router.post('/user', 
   validateDisplayName,
   validateEmail,
   validatePassword,
   controller.createUser);
+
+router.get('/user', validateToken, controller.getAllUsers);
 
 module.exports = router;
