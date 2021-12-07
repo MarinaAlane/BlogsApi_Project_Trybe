75f37e9ccb6493e8f9eec3b2b49b390aba67214a
Matheus Antonio <matheusantonio232@gmail.com>
2021-12-07 20:19:38 -0300

    add middlewares in routes

--- src/routes/blogPostRoute.js
+++ src/routes/blogPostRoute.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 const controller = require('../controllers/blogPostsController');
 const { validateToken } = require('../middlewares/validateToken');
+const { validateTitle, 
+  validateContent, 
+  validateCategorysId } = require('../middlewares/blogPostValidation');
 
-router.post('/post', validateToken, controller.createPost);
+router.post('/post', 
+  validateToken,
+  validateTitle, 
+  validateContent, 
+  validateCategorysId,
+  controller.createPost);
 
 module.exports = router;
