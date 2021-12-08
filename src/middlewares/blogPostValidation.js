1ff991ccab6abda1fbd513d4d5fe325f5d2d9214
Matheus Antonio <matheusantonio232@gmail.com>
2021-12-07 23:23:55 -0300

    feat: add authorized user validation

--- src/middlewares/blogPostValidation.js
+++ src/middlewares/blogPostValidation.js
@@ -22,8 +22,38 @@ const validateCategorysId = (req, res, next) => {
   next();
 };
 
+const validateUpdatePostNoCategories = (req, res, next) => {
+  const { categoryIds } = req.body;
+  
+  if (categoryIds) return res.status(400).json({ message: 'Categories cannot be edited' });
+  
+  next();
+};
+
+const { BlogPost, User } = require('../models');
+
+const validateAuthorizedUser = async (req, res, next) => {
+  const { id } = req.params;
+  const { id: userId } = req.user;
+
+  const post = await BlogPost.findOne({
+    where: { id },
+    include: [
+      { model: User, as: 'user', attributes: { exclude: ['password'] } },
+    ],
+  });
+
+  if (userId !== post.userId) {
+    return res.status(401).json({ message: 'Unauthorized user' });
+  }
+
+  next();
+};
+
   module.exports = { 
     validateTitle,
     validateContent,
     validateCategorysId,
+    validateUpdatePostNoCategories,
+    validateAuthorizedUser,
   };
