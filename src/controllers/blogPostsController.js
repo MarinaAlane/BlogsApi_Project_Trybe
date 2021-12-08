eb52d9e9721fdbac8aa2999c6c641b3c22b8e086
Matheus Antonio <matheusantonio232@gmail.com>
2021-12-07 21:42:03 -0300

    feat: add getPostById method on blogPostsController

--- src/controllers/blogPostsController.js
+++ src/controllers/blogPostsController.js
@@ -33,4 +33,26 @@ const getAllPosts = async (req, res) => {
   }
 };
 
-module.exports = { createPost, getAllPosts };
+const getPostById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const post = await BlogPost.findOne({
+      where: { id },
+      include: { all: true },
+    });
+
+    if (!post) { return res.status(404).json({ message: 'Post does not exist' }); }
+
+    return res.status(200).json(post);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Ocorreu um erro' });
+  }
+};
+
+module.exports = { 
+  createPost, 
+  getAllPosts,
+  getPostById,
+ };
