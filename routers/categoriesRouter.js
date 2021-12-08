bbd48d839e6d27ef29b7e07cbf84bc88e202ffcc
CamilaCode7 <camilafabiabh9@gmail.com>
2021-12-08 17:27:31 -0300

    adicionando req 6 refatorado

--- routers/categoriesRouter.js
+++ routers/categoriesRouter.js
@@ -6,5 +6,6 @@ const validateToken = require('../auth/token');
 const router = Router();
 
 router.post('/', validateToken, categoriesValidate, controller.create);
+router.get('/', validateToken, controller.getAllCategories);
 
 module.exports = router;
