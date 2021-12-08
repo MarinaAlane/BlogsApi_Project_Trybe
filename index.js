e2b85c6a9dc23b0ad4a9527670b03db4a72cead3
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-12-08 19:47:34 -0300

    feat: req-1

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const Users = require('./controllers/UserController');
+const Validations = require('./middlewares/Validations');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
-//
+// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.post('/user', [
+  Validations.validateName,
+  Validations.validateEmail,
+  Validations.validatePassword,
+  Users.createUser,
+]);
