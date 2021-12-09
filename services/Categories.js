539239e65fdec68799f43ba3a2e897f74df50ac2
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-12-08 22:35:23 -0300

    correction requirement five done

--- services/Categories.js
+++ services/Categories.js
@@ -1,12 +1,12 @@
 const { StatusCodes } = require('http-status-codes');
-const { Category } = require('../models');
+const { Categories } = require('../models');
 
 const createCategory = async ({ name }) => {
   if (!name) {
     return { status: StatusCodes.BAD_REQUEST, message: '"name" is required' };
   }
 
-  const user = await Category.createCategory({ name });
+  const user = await Categories.createCategory({ name });
   return { status: StatusCodes.CREATED, user };
 };
 
