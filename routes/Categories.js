91d8ce3bbde17f14e690445dfac023288c6fe4b3
Marcos Albuquerque <mpdsa20@gmail.com>
2021-12-09 15:51:23 -0300

    Requisito 6

--- routes/Categories.js
+++ routes/Categories.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const validateToken = require('../validations/validateToken');
 const validateCategory = require('../validations/validateCategory');
-const { createCategory } = require('../controller/Categories');
+const { createCategory, allCategories } = require('../controller/Categories');
 
 router.post('/', validateToken, validateCategory, createCategory);
+router.get('/', validateToken, allCategories);
 
 module.exports = router;
