a9b58b254297df163d1762cb42a5d41d54d7d270
Marcos Albuquerque <mpdsa20@gmail.com>
2021-12-09 09:58:52 -0300

    Fix lint

--- validations/validateRegistration.js
+++ validations/validateRegistration.js
@@ -3,27 +3,20 @@ const { MESSAGE_ERROR1, MESSAGE_ERROR2,
   MESSAGE_ERROR3, MESSAGE_ERROR4, MESSAGE_ERROR5, MESSAGE_ERROR6 } = require('./messageError');
 
 function checkPassword(res, password) {
-  if (password === undefined) {
-    sendStatusError(400, MESSAGE_ERROR3, res);
-  } else if (typeof (password) !== 'string') {
-    sendStatusError(400, MESSAGE_ERROR1, res);
-  } else if (password.length <= 5) sendStatusError(400, MESSAGE_ERROR2, res);
+  if (password === undefined) sendStatusError(400, MESSAGE_ERROR3, res);
+  if (typeof (password) !== 'string') sendStatusError(400, MESSAGE_ERROR1, res);
+  if (password.length <= 5) sendStatusError(400, MESSAGE_ERROR2, res);
 }
 
 function checkEmail(res, email, emailRegex) {
-  if (email === undefined) {
-    sendStatusError(400, MESSAGE_ERROR4, res);
-  } else if (!emailRegex.test(email)) sendStatusError(400, MESSAGE_ERROR1, res);
+  if (email === undefined) sendStatusError(400, MESSAGE_ERROR4, res);
+  if (!emailRegex.test(email)) sendStatusError(400, MESSAGE_ERROR1, res);
 }
 
 function checkDisplayName(res, displayName) {
-  if (displayName.length <= 7) {
-    sendStatusError(400, MESSAGE_ERROR6, res);
-  } else if (displayName === undefined) {
-    sendStatusError(400, MESSAGE_ERROR5, res);
-  } else if (typeof (displayName) !== 'string') {
-    sendStatusError(400, MESSAGE_ERROR1, res);
-  }
+  if (displayName.length <= 7) sendStatusError(400, MESSAGE_ERROR6, res);
+  if (displayName === undefined) sendStatusError(400, MESSAGE_ERROR5, res);
+  if (typeof (displayName) !== 'string') sendStatusError(400, MESSAGE_ERROR1, res);
 }
 
 function validateRegistration(req, res, next) {
