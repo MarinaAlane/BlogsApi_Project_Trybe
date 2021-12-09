be9ff19b0ab213258e49db32ee7cc7a6eea8f529
robsoncardozo <robsonleaocardozo@hotmail.com.br>
2021-12-09 00:41:54 -0300

    index fix

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require('express');
 // routes imports
 const Users = require('./routes/userRoutes');
 const Categories = require('./routes/categoryRoutes');
+const Post = require('./routes/postRoutes');
 
 // MW imports
 const errorMW = require('./middlewares/error');
@@ -16,6 +17,7 @@ app.use(express.json());
 // routes use
 app.use(Users);
 app.use(Categories);
+app.use(Post);
 
 // middlewares
 app.use(errorMW);
