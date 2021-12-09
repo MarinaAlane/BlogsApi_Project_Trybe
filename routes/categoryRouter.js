b4042dce6970180338c54e9e776a1a1b59db1a00
eraldoalmeida <eraldo.salmeida@gmail.com>
2021-12-09 11:49:15 -0300

    criando rota, service e controller para todas as categorias

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -5,5 +5,5 @@ const validateJWT = require('../middleware/validateJwt');
 const router = express.Router();
 
 router.post('/', validateJWT, categoryController.createCategory);
-
+router.get('/', validateJWT, categoryController.getCategories);
 module.exports = router;
